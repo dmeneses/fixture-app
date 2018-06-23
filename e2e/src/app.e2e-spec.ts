adb67bdec652db2f06fd6f26c506a5c84ad3974b
Daniela Meneses <dvargas@oildex.com>
2018-06-23 16:35:09 -0400

    Basic integration tests for demo purposes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,53 @@
+import { element } from 'protractor';
+
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('Fixture App', () => {
   let page: AppPage;
+
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to fixture-app!');
+    expect(page.getGameListTitle().getText()).toEqual('Available Games');
+  });
+
+  it('should display the list of games', () => {
+    page.navigateTo();
+    page.getGames().then(function(games) {
+      expect(games.length).toBe(3);
+      expect(games[0].getText()).toBe('Inglaterra - Espania');
+      expect(games[1].getText()).toBe('Bolivia - Brasil');
+      expect(games[2].getText()).toBe('China - Japon');
+    });
+  });
+
+  it('should display the score setter when clicking on one game', () => {
+    page.navigateTo();
+    page.clickOnGameOnIndex(1);
+    expect(page.getGameSelectedTitle().getText()).toEqual('Selected Game');
+  });
+
+  it('should increment score when clicking on the left button', () => {
+    page.navigateTo();
+    page.clickOnGameOnIndex(1);
+    page.clickOnLeftIncrementScore();
+    expect(page.getLeftScore().getText()).toEqual('1');
+    page.clickOnLeftIncrementScore();
+    expect(page.getLeftScore().getText()).toEqual('2');
+    expect(page.getRightScore().getText()).toEqual('0');
+  });
+
+  it('should increment score when clicking on the right button', () => {
+    page.navigateTo();
+    page.clickOnGameOnIndex(1);
+    page.clickOnRightIncrementScore();
+    expect(page.getRightScore().getText()).toEqual('1');
+    page.clickOnRightIncrementScore();
+    expect(page.getRightScore().getText()).toEqual('2');
+    expect(page.getLeftScore().getText()).toEqual('0');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,48 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getGameListTitle(): any {
+    return element(by.css('.game-list-title'));
+  }
+
+  getGameSelectedTitle(): any {
+    return element(by.css('.game-selected-title'));
+  }
+
+  clickOnGameOnIndex(index: any): any {
+    this.getGames().get(index).click();
+  }
+
+
+  getGames(): ElementArrayFinder {
+    return element.all(by.tagName('li'));
+  }
+
+  getRightScore(): any {
+    return element(by.css('.right-score'));
+  }
+
+  clickOnRightIncrementScore(): any {
+    this.getRightIncrementScore().click();
+  }
+
+  getLeftScore(): any {
+    return element(by.css('.left-score'));
+  }
+
+  clickOnLeftIncrementScore(): any {
+    this.getLeftIncrementScore().click();
+  }
+
+  getLeftIncrementScore(): any {
+    return element.all(by.tagName('button')).get(0);
+  }
+
+  getRightIncrementScore(): any {
+    return element.all(by.tagName('button')).get(1);
   }
 }
